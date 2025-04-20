63cbd187abb8f999213a9836a8e9a5924b8d18ee
Daniel <d.napieraj06@gmail.com>
2025-04-20 18:59:36 +0200

    creating a new log.jsx component and avoiding intersecting states

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
+import { useState } from 'react'
+
 import Player from './compontents/Player'
 import GameBoard from './compontents/GameBoard'
+import Log from './compontents/Log'
 
 function App() {
+	const [gameTurns, setGameTurns] = useState([])
+	const [activePlayer, setActivePlayer] = useState('X')
+
+	function handleSelectSquare() {
+		setActivePlayer(curActivePlayer => (curActivePlayer === 'X' ? 'O' : 'X'))
+		setGameTurns()
+	}
+
 	return (
 		<main>
 			<div id="game-container">
-				<ol id="players">
-					<Player initialName="Player 1" symbol="X" />
-					<Player initialName="Player 2" symbol="O" />
+				<ol id="players" className="highlight-player">
+					<Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+					<Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
 				</ol>
-				<GameBoard />
+				<GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
 			</div>
-			LOG
+			<Log />
 		</main>
 	)
 }
